64c8e934ef07cc0e7c903fee87ff1cc369f1c73f
oereo <dlstpgns0406@gmail.com>
2020-08-16 23:20:34 +0900

    [MODIFY] Login

--- mainPage/urls.py
+++ mainPage/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
  
 urlpatterns = [
     path("login", views.login, name="login"),
+    path("logout", views.logout, name="logout"),
+
 ]
--- mainPage/views.py
+++ mainPage/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "POST":
+        username = request.POST.get('ID')
+        password = request.POST.get('PASSWORD')
+        user = auth.authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            print(user.username + " login")
+            return redirect('/',user)
+        else:
+            return render(request, 'login.html',{'error':'username or password is incorrect.'})
+    else:
+        return render(request,'login.html')
+
+
+def logout(request):
+    if request.method == 'GET':
+        auth.logout(request)
+        print('log out success')
+        return redirect('/')
+    return render(request,'login.html')
